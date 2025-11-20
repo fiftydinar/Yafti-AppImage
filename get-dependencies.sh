8ae4d2b2fdb8be4b25e45f202f6cf6ff13def9d8
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-20 13:06:24 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,12 +8,12 @@ echo "Building yafti..."
 echo "---------------------------------------------------------------"
 sed -i 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
 git clone https://github.com/ublue-os/yafti.git ./yafti && (
-	cd ./yafti
-  mv ./pkg/PKGBUILD ./PKGBUILD
-  sed -i 's/^pkgver=.*/pkgver=0.10.2/' ./PKGBUILD
-  makepkg -fs --noconfirm
-	ls -la .
-	pacman --noconfirm -U *.pkg.tar.*
+    cd ./yafti
+    mv ./pkg/PKGBUILD ./PKGBUILD
+    sed -i 's/^pkgver=.*/pkgver=0.10.2/' ./PKGBUILD
+    makepkg -fs --noconfirm
+    ls -la .
+    pacman --noconfirm -U *.pkg.tar.*
 )
 
 echo "Installing debloated packages..."
