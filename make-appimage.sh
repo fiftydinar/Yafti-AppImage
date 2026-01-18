658b24952b4e9137b8c101a1924454229c8702cf
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-18 20:53:09 +0100

    Set `MAIN_BIN` to default to `yafti` instead to `python`

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,12 +9,13 @@ export OUTPATH=./dist
 # export ADD_HOOKS="self-updater.bg.hook" # This is excluded because it's going to be used in immutable directories
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export APPNAME=yafti
+export MAIN_BIN=yafti
 export ICON=DUMMY
 export DESKTOP=DUMMY
 export DEPLOY_SYS_PYTHON=1
-export DEPLOY_OPENGL=1
 export DEPLOY_GTK=1
 export GTK_DIR=gtk-4.0
+export ANYLINUX_LIB=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/yafti -- ./yafti-config.yml
